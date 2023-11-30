8ce7a4c957e1db906c01525adcf9682156cc22f5
Vladislav Bogatyrev <vladislav.bogatyrev@gmail.com>
2023-11-30 23:26:38 +0300

    new: add Prisma + Swagger initials configs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4566);
+  const options = new DocumentBuilder()
+    .setTitle('SINS')
+    .setDescription('API server for Grimoire | HR System')
+    .setVersion('1.0')
+    .addServer('http://localhost:4566/', 'Local environment')
+    .addServer('https://sins.v-b.tech/', 'Production')
+    .addTag('Backend')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(process.env.PORT || 4566);
 }
 bootstrap();
